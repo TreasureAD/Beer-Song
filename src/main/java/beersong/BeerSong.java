1522a1fd817219cb0ba31c3c1072fd66310c8d83
TreasureAD <treasuredavis111@gmail.com>
2024-08-30 08:39:22 -0400

    Beer song code

---
+++ src/main/java/beersong/BeerSong.java
@@ -0,0 +1,32 @@
+//Treasure Davis
+//Java Homework
+//Detected flaw: The transition from "bottles" to "bottle"
+
+package beersong;
+
+public class BeerSong {
+    public static void main(String[] args) {
+        int beerNum = 99;
+        String word = "bottles";
+
+        while (beerNum > 0) {
+
+            System.out.println(beerNum + " " + word + " of beer on the wall");
+            System.out.println(beerNum + " " + word + " of beer.");
+            System.out.println("Take one down.");
+            System.out.println("Pass it around.");
+            beerNum = beerNum - 1;
+
+            if (beerNum == 1) {
+                word = "bottle"; //singular, one bottle
+            } //Moved from above the while loop to fix the flaw
+
+            if (beerNum > 0) {
+                System.out.println(beerNum + " " + word + " of beer on the wall");
+            } else {
+                System.out.println("No more bottles of beer on the wall");
+            } //end else loop
+        } // end while loop
+    } // end main
+} // end class
+
